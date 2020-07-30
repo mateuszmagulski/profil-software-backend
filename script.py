09b85d4b5ec2f9da66ac8e21dd2fedd1b318d548
mateuszmagulski <mateusz.magulski@gmail.com>
2020-07-30 16:58:27 +0200

    class/static methods, connect to db

--- script.py
+++ script.py
@@ -1,16 +1,22 @@
 import argparse
 from datetime import datetime
+from peewee import *
+
+db = SqliteDatabase("people.db")
+db.connect()
 
 
 class App:
-    def valid_date(self, date):
+    @staticmethod
+    def valid_date(date):
         try:
             return datetime.strptime(date, "%d-%m-%Y")
         except ValueError:
             msg = "Not a valid date: {0}".format(date)
             raise argparse.ArgumentTypeError(msg)
 
-    def __call__(self):
+    @classmethod
+    def parse(cls):
         parser = argparse.ArgumentParser()
         parser.add_argument("-a", "--api", type=int, help="Add N records from API")
         parser.add_argument(
@@ -38,7 +44,7 @@ class App:
         parser.add_argument(
             "-d",
             "--date",
-            type=self.valid_date,
+            type=App.valid_date,
             nargs=2,
             help="Users borned between two dates in format DD-MM-YYYY",
         )
@@ -48,5 +54,44 @@ class App:
         args = parser.parse_args()
 
 
-app = App()
-app()
+class Person(Model):
+
+    gender = CharField()
+    name_title = CharField()
+    name_first = CharField()
+    name_last = CharField()
+    location_street_number = CharField()
+    location_street_name = CharField()
+    location_city = CharField()
+    location_state = CharField()
+    location_country = CharField()
+    location_postcode = CharField()
+    location_coordinates_latitude = CharField()
+    location_coordinates_longitude = CharField()
+    location_timezone_offset = CharField()
+    location_timezone_description = CharField()
+    email = CharField()
+    login_uuid = CharField()
+    login_username = CharField()
+    login_password = CharField()
+    login_salt = CharField()
+    login_md5 = CharField()
+    login_sha1 = CharField()
+    login_sha256 = CharField()
+    dob_date = DateTimeField()
+    dob_age = IntegerField()
+    dob_next = IntegerField()
+    registered_date = DateTimeField()
+    registered_age = IntegerField()
+    phone = IntegerField()
+    cell = IntegerField()
+    id_name = CharField(null=True)
+    id_value = CharField(null=True)
+    nat = CharField()
+
+    class Meta:
+        database = db
+
+
+App.parse()
+db.close()
